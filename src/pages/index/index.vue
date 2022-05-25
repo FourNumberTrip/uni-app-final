bdaa4079db35d480fa57226fcc645c07d9d14264
xyqyear <215908558@qq.com>
2022-05-25 13:09:35 +0800

    add /guide page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,18 @@
 <template>
   <view class="content">
-    <navigator
-      url="/pages/select/select"
-      hover-class="navigator-hover"
-    >
+    <navigator url="/pages/guide/guide" hover-class="navigator-hover">
+      <button type="default">引导界面</button>
+    </navigator>
+    <navigator url="/pages/select/select" hover-class="navigator-hover">
       <button type="default">选择页面</button>
     </navigator>
-	<navigator
-      url="/pages/list/list"
-      hover-class="navigator-hover"
-    >
+    <navigator url="/pages/list/list" hover-class="navigator-hover">
       <button type="default">列表页面</button>
     </navigator>
-	<navigator
-      url="/pages/pain/pain"
-      hover-class="navigator-hover"
-    >
+    <navigator url="/pages/pain/pain" hover-class="navigator-hover">
       <button type="default">疼痛选择页面</button>
     </navigator>
-	<navigator
-      url="/pages/action/action"
-      hover-class="navigator-hover"
-    >
+    <navigator url="/pages/action/action" hover-class="navigator-hover">
       <button type="default">动画页面</button>
     </navigator>
   </view>
