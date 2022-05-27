83f52a33ddcc3bf5c1eadc2d94c404649b64232a
xyqyear <xyqyear@gmail.com>
2022-05-27 15:58:05 +0800

    add expand to next page animation

--- src/pages/list/list.vue
+++ src/pages/list/list.vue
@@ -8,9 +8,15 @@
         :key="index"
         :data-index="index"
       >
-        <image class="cover-image" :src="item.coverUrl"></image>
-        <text>{{ item.title }}</text>
-        <text>item id: {{ index }}</text>
+        <view class="list-item-content">
+          <view class="placeholder"></view>
+          <view class="image-container">
+            <img class="cover-image" :src="item.coverUrl" />
+          </view>
+          <view class="text">
+            <text>{{ item.title + index }}</text>
+          </view>
+        </view>
       </view>
     </view>
   </view>
@@ -28,13 +34,6 @@ export default {
           description: "上身活动",
           animationSetId: 0,
           // for animation
-          class: "list-item-out-bottom",
-        },
-        {
-          coverUrl: "/static/logo.png",
-          title: "上身活动",
-          description: "上身活动",
-          animationSetId: 0,
           class: "list-item-out-bottom",
         },
         {
@@ -149,7 +148,7 @@ export default {
           //   // appear from top
           // } else {
           //   // appear from bottom
-          // }
+          // }r
         } else {
           // disappear
           if (res.boundingClientRect.top < 0) {
@@ -168,7 +167,9 @@ export default {
     }
   },
   methods: {
-    onListItemClick(index) {},
+    onListItemClick(index) {
+      this.items[index].class = "list-item-expanding";
+    },
   },
 };
 </script>
@@ -179,66 +180,104 @@ $list-item-card-color: white;
 
 .content {
   background: $list-background-color;
+
+  .list {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+
+    .list-item {
+      display: flex;
+      width: 330rpx;
+      height: 330rpx;
+      background: $list-item-card-color;
+      border-radius: 20rpx;
+      margin: 12rpx;
+      box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+
+      transition: margin-top 600ms, margin-bottom 600ms, opacity 600ms,
+        transform 200ms;
+
+      .list-item-content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        .placeholder {
+          flex: 1;
+        }
+
+        .image-container {
+          flex: 1;
+          .cover-image {
+            width: 200rpx;
+            height: 200rpx;
+          }
+        }
+
+        .text {
+          flex: 2;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 36rpx;
+        }
+      }
+    }
+
+    .list-item:active {
+      pointer-events: none;
+      transform: scale(0.96);
+    }
+
+    .list-item:hover {
+      transform: scale(0.96);
+    }
+
+    .list-item-out-bottom {
+      margin-top: 60rpx !important;
+      opacity: 0;
+    }
+
+    .list-item-out-top {
+      margin-bottom: 60rpx !important;
+      opacity: 0;
+    }
+  }
 }
 
-.list {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
+// list item expanding animation when transitioning to the next page
+@keyframes list-item-expanding-animation {
+  from {
+    scale: 0.96;
+  }
+  to {
+    scale: 10;
+  }
 }
+.list-item-expanding {
+  animation: list-item-expanding-animation 400ms linear forwards;
 
-.list-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  @keyframes list-item-content-hidden-animation {
+    from {
+      opacity: 1;
+    }
 
-  width: 330rpx;
-  height: 330rpx;
-  background: $list-item-card-color;
-  border-radius: 20rpx;
-  margin: 12rpx;
+    20% {
+      opacity: 0;
+    }
 
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-
-  transition: margin-top 600ms, margin-bottom 600ms, opacity 600ms,
-    transform 200ms;
-}
-
-.list-item:active {
-  pointer-events: none;
-  transform: scale(0.96);
-}
-
-.list-item:hover {
-  transform: scale(0.96);
-}
-
-.list-item-out-bottom {
-  margin-top: 60rpx !important;
-  opacity: 0;
-}
-
-.list-item-out-top {
-  margin-bottom: 60rpx !important;
-  opacity: 0;
-}
-
-.cover-image {
-  width: 200rpx;
-  height: 200rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+    to {
+      opacity: 0;
+    }
+  }
+  .list-item-content {
+    animation: list-item-content-hidden-animation 400ms linear forwards;
+  }
 }
 </style>
