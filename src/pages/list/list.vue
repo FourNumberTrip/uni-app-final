6c6bdab37e0ecbdbceaa9353357411fa9cf4eee7
xyqyear <xyqyear@gmail.com>
2022-05-30 14:07:20 +0800

    change the style of list item to be translucent layer on top of an image

--- src/pages/list/list.vue
+++ src/pages/list/list.vue
@@ -8,13 +8,10 @@
         :key="index"
         :data-index="index"
       >
-        <view class="list-item-content">
-          <view class="placeholder"></view>
-          <view class="image-container">
-            <img class="cover-image" :src="item.coverUrl" />
-          </view>
-          <view class="text">
-            <text>{{ item.title + index }}</text>
+        <img class="image" :src="item.coverUrl" />
+        <view class="translucent-box">
+          <view class="title">
+            <text>{{ item.title }}</text>
           </view>
         </view>
       </view>
@@ -29,16 +26,16 @@ export default {
     return {
       items: [
         {
-          coverUrl: "/static/logo.png",
-          title: "上身活动",
+          coverUrl: "/static/py.png",
+          title: "抛秧",
           description: "上身活动",
           animationSetId: 0,
           // for animation
           class: "list-item-out-bottom",
         },
         {
-          coverUrl: "/static/logo.png",
-          title: "上身活动",
+          coverUrl: "/static/cy.png",
+          title: "插秧",
           description: "上身活动",
           animationSetId: 0,
           class: "list-item-out-bottom",
@@ -175,8 +172,7 @@ export default {
 </script>
 
 <style lang="scss">
-$list-background-color: #f3f5f8;
-$list-item-card-color: white;
+$list-background-color: white;
 
 .content {
   background: $list-background-color;
@@ -190,42 +186,42 @@ $list-item-card-color: white;
     width: 100%;
 
     .list-item {
-      display: flex;
-      width: 330rpx;
-      height: 330rpx;
-      background: $list-item-card-color;
-      border-radius: 20rpx;
-      margin: 12rpx;
-      box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+      position: relative;
+      width: 320rpx;
+      height: 320rpx;
+      border-radius: 30rpx;
+      margin: 18rpx;
+      box-shadow: rgba(0, 0, 0, 0.19) 0rpx 10rpx 20rpx,
+        rgba(0, 0, 0, 0.23) 0rpx 6rpx 6rpx;
 
       transition: margin-top 600ms, margin-bottom 600ms, opacity 600ms,
         transform 200ms;
 
-      .list-item-content {
-        flex: 1;
+      .image {
+        z-index: 0;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        border-radius: 30rpx;
+      }
+
+      .translucent-box {
+        z-index: 1;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        border-radius: 30rpx;
+        background: rgba(0, 0, 0, 0.25);
+
         display: flex;
         flex-direction: column;
-        align-items: center;
         justify-content: center;
+        align-items: center;
 
-        .placeholder {
-          flex: 1;
-        }
-
-        .image-container {
-          flex: 1;
-          .cover-image {
-            width: 200rpx;
-            height: 200rpx;
-          }
-        }
-
-        .text {
-          flex: 2;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          font-size: 36rpx;
+        .title {
+          padding-top: 150rpx;
+          font-size: 50rpx;
+          color: #ffffff;
         }
       }
     }
@@ -254,9 +250,11 @@ $list-item-card-color: white;
 // list item expanding animation when transitioning to the next page
 @keyframes list-item-expanding-animation {
   from {
+    z-index: 3;
     scale: 0.96;
   }
   to {
+    z-index: 3;
     scale: 10;
   }
 }
