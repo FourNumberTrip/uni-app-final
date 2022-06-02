b50a43267aaae34c679e2223a2211075af3fefb3
xyqyear <215908558@qq.com>
2022-06-02 16:28:44 +0800

    add the top progress bar add the top timer add the bottom progress bar tried to add icons better style

--- src/pages/action/action.vue
+++ src/pages/action/action.vue
@@ -1,5 +1,23 @@
 <template>
   <view class="content">
+    <view class="overall-progressbar">
+      <view
+        class="overall-progressbar-parts"
+        v-for="(animation, index) in currentAnimations"
+        :key="index"
+      >
+        <view
+          class="progress"
+          :style="{
+            width: `${Math.round(completedPercentages[index] * 100)}%`,
+          }"
+        ></view>
+      </view>
+    </view>
+    <view class="overall-timer-area">
+      <text class="overall-timer">{{ currentOverallTime }}</text>
+      <text class="overall-full-time">{{ totalTime }}</text>
+    </view>
     <canvas
       class="webgl"
       type="webgl"
@@ -7,34 +25,22 @@
       @touchstart="onTX"
       @touchmove="onTX"
       @touchend="onTX"
-      :style="{ height: screenHeight + 'px' }"
     ></canvas>
-
-    <button @click="turnBack" style="margin-top: 0px;z-index: 1">直接转</button>
-    <button @click="autoTurn" style="margin-top: 0px;z-index: 1">动画转</button>
-    <button @click="reset" style="margin-top: 0px;z-index: 1">复位</button>
-    <button @click="setSpeed" style="margin-top: 0px;z-index: 1">
-      <text v-if="lowSpeed==false">0.5倍速</text>
-      <text v-else>常速</text>
-    </button>
-    <button @click="pause" style="margin-top: 0px;z-index: 1">
-      <text v-if="paused==false">暂停</text>
-      <text v-else>播放</text>
-    </button>
-
-
-    <text class="action_name">肩部绕环</text>
-
-    <view class="list">
-      <view
-        class="list-item"
-        v-for="(item, index) in items"
-        :key="item.title"
-        @ontouchstart="onTouchStart(index)"
-        @touchend="onTouchEnd(index)"
-      >
-        <img class="image" :src="item.image" />
+    <view class="control-area">
+      <view class="background-progress">
+        <view class="progress"></view>
       </view>
+      <view class="controls">
+        <view
+          class="material-icon previous"
+          data-color="black"
+          data-icon="skip-previous"
+        ></view>
+        <view class="spacer"></view>
+        <view class="pause"></view>
+        <view class="space"></view>
+        <view class="next"></view
+      ></view>
     </view>
   </view>
 </template>
@@ -48,255 +54,406 @@ import {
   PerspectiveCamera,
   Scene,
   sRGBEncoding,
-  Vector3,
   WebGL1Renderer,
 } from "three";
+
 import { WechatPlatform, PlatformManager } from "platformize-three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// for animation control
 let clock = new Clock();
+// for animation
 let mixer;
-let gltfLoader;
 let controls;
 let renderer;
 let canvas;
 let scene;
 let camera;
 let platform;
-let disposing = false;
-let frameId = -1;
-let activeAction=[];
+// animations
+let activeAction = [];
+
+// convert seconds to mm:ss format
+function secondsToTimeString(totalSeconds) {
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = Math.floor(totalSeconds % 60);
+  return `${minutes < 10 ? "0" : ""}${minutes}:${
+    seconds < 10 ? "0" : ""
+  }${seconds}`;
+}
 
 export default {
   data() {
     return {
-      screenHeight : "",
-      turning:false,
-      cnt_turn:0,
-      cur_idx:0,
-      paused:false,
-      lowSpeed:false,
-      url:"https://threejs.org/examples/models/gltf/RobotExpressive/RobotExpressive.glb",
+      turning: false,
+      cnt_turn: 0,
+      paused: false,
+      lowSpeed: false,
+      url: "https://threejs.org/examples/models/gltf/RobotExpressive/RobotExpressive.glb",
       items: [
         {
           image: "/static/small.png",
-          title:"action1",
+          title: "action1",
           touchDown: false,
         },
         {
           image: "/static/small.png",
-          title:"action2",
+          title: "action2",
           touchDown: false,
         },
         {
           image: "/static/small.png",
-          title:"action3",
+          title: "action3",
           touchDown: false,
         },
         {
           image: "/static/small.png",
-          title:"action4",
+          title: "action4",
           touchDown: false,
         },
       ],
+      animationDurations: [],
+
+      currentAnimationIndex: 0,
+      currentAnimations: [
+        {
+          id: 0,
+          loopTimes: 8,
+        },
+        {
+          id: 2,
+          loopTimes: 8,
+        },
+        {
+          id: 4,
+          loopTimes: 8,
+        },
+      ],
+      currentPlayingTime: 0,
     };
   },
   onLoad() {
     // this.load("https://egg.moe/custom/untitled1.glb")
   },
   mounted() {
-    this.screenHeight = uni.getSystemInfoSync().windowHeight
-    this.load(this.url)
+    // this.screenHeight = uni.getSystemInfoSync().windowHeight;
+    this.load(this.url);
+  },
+  computed: {
+    currentAnimationId() {
+      return this.currentAnimations[this.currentAnimationIndex].id;
+    },
+    completedAnimations() {
+      return this.currentAnimations.map((_, index) => {
+        return index < this.currentAnimationIndex ? true : false;
+      });
+    },
+    currentAnimationDurations() {
+      return this.currentAnimations.map(
+        (i) => this.animationDurations[i.id] * i.loopTimes
+      );
+    },
+    completedPercentages() {
+      return this.completedAnimations.map((completed, index, l) => {
+        if (completed) {
+          return 1;
+          // if it's the first or if the previous one is completed and the current one is not
+          // then the current one is running
+        } else if (index == 0 || (l[index - 1] && !completed)) {
+          return (
+            this.currentPlayingTime / this.currentAnimationDurations[index]
+          );
+        } else {
+          return 0;
+        }
+      });
+    },
+    totalTime() {
+      return secondsToTimeString(
+        this.currentAnimationDurations.reduce((a, b) => a + b, 0)
+      );
+    },
+    currentOverallTime() {
+      const currentOverallSeconds =
+        this.currentAnimationDurations
+          .filter((_, index) => this.completedAnimations[index])
+          .reduce((a, b) => a + b, 0) + this.currentPlayingTime;
+      return secondsToTimeString(currentOverallSeconds);
+    },
   },
   methods: {
-    pause(){
-      let cur_action=activeAction[this.cur_idx]
-      if(this.paused){
-        cur_action.paused=false
-        this.paused=false
-      }else{
-        cur_action.paused=true
-        this.paused=true
+    pause() {
+      let cur_action = activeAction[this.currentAnimationId];
+      if (this.paused) {
+        cur_action.paused = false;
+        this.paused = false;
+        // this controls the animation
+        clock.start();
+      } else {
+        cur_action.paused = true;
+        this.paused = true;
+
+        clock.stop();
       }
     },
-    setSpeed(){
-      if(this.lowSpeed){
-        mixer.timeScale=1
-        this.lowSpeed=false
-      }else{
-        mixer.timeScale=0.5
-        this.lowSpeed=true
+    setSpeed() {
+      if (this.lowSpeed) {
+        mixer.timeScale = 1;
+        this.lowSpeed = false;
+      } else {
+        mixer.timeScale = 0.5;
+        this.lowSpeed = true;
       }
     },
-    reset(){
-      controls.reset()
+    reset() {
+      controls.reset();
     },
-    action(index){
-            activeAction[this.cur_idx].stop();
-            activeAction[index].play();
-            this.cur_idx=index;
+    action(index) {
+      activeAction[this.currentAnimationId].stop();
+      this.currentAnimationIndex = index;
+      // make sure currentAnimationId is updated
+      this.$nextTick(() => {
+        activeAction[this.currentAnimationId].play();
+      });
+      mixer.time = 0;
     },
-    load(url){
-      console.log("loading:")
-      console.log(url)
+    load(url) {
+      console.log("loading:");
+      console.log(url);
       uni
-          .createSelectorQuery()
-          .select("#gl")
-          .node()
-          .exec((res) => {
-            canvas = res[0].node;
+        .createSelectorQuery()
+        .select("#gl")
+        .node()
+        .exec((res) => {
+          canvas = res[0].node;
 
-            platform = new WechatPlatform(canvas);
-            console.log(platform);
-            PlatformManager.set(platform);
+          platform = new WechatPlatform(canvas);
+          console.log(platform);
+          PlatformManager.set(platform);
 
-            renderer = new WebGL1Renderer({
-              canvas,
-              antialias: true,
-              alpha: true,
-            });
+          renderer = new WebGL1Renderer({
+            canvas,
+            antialias: true,
+            alpha: true,
+          });
 
-            camera = new PerspectiveCamera(
-                45,
-                canvas.width / canvas.height,
-                1,
-                2000
-            );
+          camera = new PerspectiveCamera(
+            45,
+            canvas.width / canvas.height,
+            0.1,
+            2000
+          );
 
-            camera.position.set(0, 2, 4);
-            camera.lookAt(new Vector3(0, 0, 0));
-            scene = new Scene();
-            gltfLoader = new GLTFLoader();
-            controls = new OrbitControls(camera, canvas);
-            controls.enableDamping = true;
-            uni.request({
-              url: url,
-              // url: "https://threejs.org/examples/models/gltf/RobotExpressive/RobotExpressive.glb",
-              // url: "https://egg.moe/custom/untitled1.glb",
-              responseType: "arraybuffer",
-              success: (res) => {
-                gltfLoader.parse(res.data, "", (gltf) => {
-                  gltf.parser = null;
-                  gltf.scene.position.y = -1;
-                  scene.add(gltf.scene);
-                  mixer = new AnimationMixer(gltf.scene);
-                  for (let i=0;i<gltf.animations.length;i++){
-                    activeAction[i]=mixer.clipAction(gltf.animations[i])
-                  }
-                  activeAction[this.cur_idx].play();
-                });
-              },
-            });
-            // camera.position.z = 10;
-            renderer.outputEncoding = sRGBEncoding;
-            scene.add(new AmbientLight(0xffffff, 1.0));
-            scene.add(new DirectionalLight(0xffffff, 1.0));
-            renderer.setSize(canvas.width, canvas.height);
-            uni.getSystemInfo({
-              success: (res) => {
-                renderer.setPixelRatio(res.pixelRatio);
-              },
-            });
+          camera.position.set(0, 0, 8);
+          scene = new Scene();
+          controls = new OrbitControls(camera, canvas);
+          controls.enableDamping = true;
+          uni.request({
+            url: url,
+            // url: "https://threejs.org/examples/models/gltf/RobotExpressive/RobotExpressive.glb",
+            // url: "https://egg.moe/custom/untitled1.glb",
+            responseType: "arraybuffer",
+            success: (res) => {
+              const gltfLoader = new GLTFLoader();
+              gltfLoader.parse(res.data, "", (gltf) => {
+                gltf.parser = null;
+                gltf.scene.position.y = -2.2;
+                scene.add(gltf.scene);
+                mixer = new AnimationMixer(gltf.scene);
+                for (const animation of gltf.animations) {
+                  activeAction.push(mixer.clipAction(animation));
+                  this.animationDurations.push(animation.duration);
+                }
+                activeAction[this.currentAnimationId].play();
+              });
+            },
+          });
+          renderer.outputEncoding = sRGBEncoding;
+          scene.add(new AmbientLight(0xffffff, 1.0));
+          scene.add(new DirectionalLight(0xffffff, 1.0));
+          renderer.setSize(canvas.width, canvas.height);
+          uni.getSystemInfo({
+            success: (res) => {
+              renderer.setPixelRatio(res.pixelRatio);
+            },
+          });
 
-            const render = () => {
-              if(this.turning) this.turn(0.01);
-              if (mixer) mixer.update(clock.getDelta());
-              if (!disposing)
-                frameId = canvas.requestAnimationFrame(render);
+          const render = () => {
+            if (this.turning) this.turn(0.01);
+            const frameId = canvas.requestAnimationFrame(render);
+            if (!this.paused) {
               controls.update();
               renderer.render(scene, camera);
-            };
-            render();
-          });
+              if (mixer) {
+                mixer.update(clock.getDelta());
+                if (frameId % 20 == 0) {
+                  this.currentPlayingTime = mixer.time;
+                }
+              }
+            }
+          };
+          render();
+        });
     },
-    onTouchStart(index) {
+    onClick(index) {
+      this.action(index);
+      console.log("onClick", index);
+      console.log(index);
     },
-    onTouchEnd(index) {
-      this.action(index)
-      this.cur_idx=index
-      console.log("onTouchEnd")
-      console.log(index)
+    onClickPushDown(e) {
+      this.canvasPusherVisible = "canvas-pusher";
+    },
+    turnBack() {
+      console.log(scene);
+      scene.rotateY(Math.PI);
+    },
+    autoTurn() {
+      this.cnt_turn = 0;
+      this.turning = true;
+    },
+    turn(step) {
+      // let step=0.01
+      controls.update();
+      renderer.render(scene, camera);
+      scene.rotateY(step * Math.PI);
+      this.cnt_turn += 1;
+      if (this.cnt_turn >= 1 / step) {
+        this.turning = false;
+      }
     },
     onTX(e) {
       platform.dispatchTouchEvent(e);
     },
-    turnBack(){
-      console.log(scene)
-      scene.rotateY(Math.PI);
-    },
-    autoTurn(){
-      this.cnt_turn=0
-      this.turning=true;
-    },
-    turn(step){
-      // let step=0.01
-      controls.update();
-      renderer.render(scene, camera);
-      scene.rotateY(step*Math.PI)
-      this.cnt_turn+=1;
-      if(this.cnt_turn>=(1/step)){
-        this.turning=false;
-      }
-    }
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "@/lib/material-icons.scss";
+
+page {
+  height: 100%;
+}
+
 .content {
+  height: 100%;
+
   display: flex;
   flex-direction: column;
-
-  height: 500px;
-  align-content: center;
-}
-.webgl {
-  /*z-index: 0;*/
-  width: 100%;
-  height: 100%;
-}
-.action_name {
-  position: fixed;
-  margin-left: 10%;
-  margin-top: 380px;
-}
-.list {
-  /*z-index: 1;*/
-  position: absolute;
-  height: 10%;
-  width: 100%;
-  display: flex;
-  flex-direction: row;
   justify-content: center;
-  position: fixed;
-  margin-top: 420px;
-  /*margin-left:10%;*/
+  align-items: center;
+
+  .overall-progressbar {
+    flex: 0;
+    margin-top: 5rpx;
+    height: 10rpx;
+    width: 99%;
+    border-radius: 5rpx;
+    border: 1rpx solid rgb(223, 223, 223);
+
+    display: flex;
+    flex-direction: row;
+
+    .overall-progressbar-parts {
+      flex: 1;
+      height: 10rpx;
+      margin: 0rpx 1rpx;
+
+      .progress {
+        background: rgb(153, 153, 153);
+        border-radius: 5rpx;
+        height: 100%;
+        float: left;
+      }
+    }
+  }
+
+  .overall-timer-area {
+    flex: 0;
+    width: 100%;
+
+    .overall-timer {
+      float: left;
+      font-size: 50rpx;
+      font-weight: bold;
+      margin-left: 10rpx;
+    }
+
+    .overall-full-time {
+      float: right;
+      font-size: 50rpx;
+      font-weight: bold;
+      margin-right: 10rpx;
+    }
+  }
+
+  .webgl {
+    flex: 8;
+    /*z-index: 0;*/
+    width: 100%;
+  }
+
+  .control-area {
+    flex: 2;
+    width: 98%;
+    margin-bottom: 10rpx;
+    position: relative;
+
+    .background-progress {
+      position: absolute;
+
+      width: 100%;
+      height: 100%;
+      border-radius: 400rpx;
+      overflow: hidden;
+
+      .progress {
+        float: left;
+        width: 60%;
+        height: 100%;
+        background: rgb(174, 174, 255);
+      }
+    }
+
+    .controls {
+      position: absolute;
+
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+
+      @include material-icon(
+        "black",
+        #000000,
+        "skip-previous",
+        $material-icon-skip-previous
+      );
+      .previous {
+        flex: 5;
+        height: 30rpx;
+        width: 30rpx;
+      }
+
+      .spacer {
+        flex: 1
+      }
+
+      .pause {
+        flex: 5;
+        height: 30rpx;
+      }
+
+      .next {
+        flex: 5;
+        height: 30rpx;
+      }
+    }
+  }
 }
 
-.list-item {
-  width: 34px;
-  /*z-index: 1;*/
-  transition: all 200ms;
-  touch-action: manipulation;
-  position: relative;
-  margin-left: 20px;
-}
-
-.list-item:active {
-  pointer-events: none;
-  transform: scale(0.9);
-}
-
-.list-item:hover {
-  transform: scale(0.9);
-}
-
-.image {
-  /*z-index: 1;*/
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  border-radius: 3px;
-  box-shadow: rgba(0, 0, 0, 0.19) 0px 5px 10px, rgba(0, 0, 0, 0.23) 0px 3px 3px;
+hidden {
+  display: hidden;
 }
 </style>
