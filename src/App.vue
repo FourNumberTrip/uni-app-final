0b250b6ab8cab001122f7593e648668312c02662
xyqyear <215908558@qq.com>
2022-06-18 11:00:36 +0800

    add dependency for tensorflow.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+import * as fetchWechat from "fetch-wechat";
+import * as tf from "@tensorflow/tfjs-core";
+import * as webgl from "@tensorflow/tfjs-backend-webgl";
+
+const plugin = requirePlugin("tfjsPlugin");
+
+export default {
+  onLaunch: function () {
+    console.log("App Launch");
+    plugin.configPlugin({
+      // polyfill fetch function
+      fetchFunc: fetchWechat.fetchFunc(),
+      // inject tfjs runtime
+      tf,
+      // inject webgl backend
+      webgl,
+      // provide webgl canvas
+      canvas: wx.createOffscreenCanvas(),
+    });
+  },
+  onShow: function () {
+    console.log("App Show");
+  },
+  onHide: function () {
+    console.log("App Hide");
+  },
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
 </style>
