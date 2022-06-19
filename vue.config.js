6b8735128cac380f5f101f264dda5aaa5deecaab
xyqyear <215908558@qq.com>
2022-06-19 15:04:59 +0800

    maybe better way to introduce wx cloud functions

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,46 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new CopyWebpackPlugin([
-                {
-                    from: path.join(__dirname, 'src/functions'),
-                    to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'functions')
-                },
-                {
-                    from: path.join(__dirname, 'project.config.json'),
-                    to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'project.config.json')
-                },
-                {
-                    from: path.join(__dirname, 'project.private.config.json'),
-                    to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'project.private.config.json')
-                }
-            ])
-        ],
-        optimization: {
-            minimize: true,
-            minimizer: [new TerserPlugin()],
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([
+        {
+          from: path.join(__dirname, "src/functions"),
+          to: path.join(
+            __dirname,
+            "dist",
+            process.env.NODE_ENV === "production" ? "build" : "dev",
+            process.env.UNI_PLATFORM,
+            "functions"
+          ),
         },
-    }
-}
+        // {
+        //   from: path.join(__dirname, "project.config.json"),
+        //   to: path.join(
+        //     __dirname,
+        //     "dist",
+        //     process.env.NODE_ENV === "production" ? "build" : "dev",
+        //     process.env.UNI_PLATFORM,
+        //     "project.config.json"
+        //   ),
+        // },
+        // {
+        //   from: path.join(__dirname, "project.private.config.json"),
+        //   to: path.join(
+        //     __dirname,
+        //     "dist",
+        //     process.env.NODE_ENV === "production" ? "build" : "dev",
+        //     process.env.UNI_PLATFORM,
+        //     "project.private.config.json"
+        //   ),
+        // },
+      ]),
+    ],
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    },
+  },
+};
