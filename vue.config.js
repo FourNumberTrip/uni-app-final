7a00eb8d4c9d26f27a55224b8d818d9410266878
xyqyear <215908558@qq.com>
2022-06-22 13:10:00 +0800

    reduce the size of the bundle

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,7 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   configureWebpack: {
-    plugins: [
-      new CopyWebpackPlugin([
-        {
-          from: path.join(__dirname, "src/functions"),
-          to: path.join(
-            __dirname,
-            "dist",
-            process.env.NODE_ENV === "production" ? "build" : "dev",
-            process.env.UNI_PLATFORM,
-            "functions"
-          ),
-        },
-      ]),
-    ],
     optimization: {
       minimize: true,
       minimizer: [new TerserPlugin()],
